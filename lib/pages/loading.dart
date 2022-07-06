5d0775faeef676221223d7ecd38aca4d3fbbee8e
Just_Kingsley <kingsleybingala@gmail.com>
2022-07-06 09:30:44 +0200

    spin kit loading working

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:world_time_app/services/world_time.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 
 
@@ -36,11 +37,14 @@ class _LoadingState extends State<Loading> {
   }
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(50.0),
-        child: Text(time),
-      )
-    );
+    return const Scaffold(
+      backgroundColor: Colors.blueAccent,
+      body: Center(
+        child: SpinKitDualRing(
+          color: Colors.white,
+          size: 50.0,
+        ),
+      ),
+      );
   }
 }
